56c56813773a485fafe3dc6517da54b874f3adce
nanandt <rizkyfatih007@gmail.com>
2021-04-26 13:19:56 +0700

    math class  dan big number

---
+++ src/rizky/classes/MathApp.java
@@ -0,0 +1,15 @@
+package rizky.classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(5000,700);
+        System.out.println(min);
+
+        var max = Math.max(5000,700);
+        System.out.println(max);
+
+        var pi = Math.PI;
+        var jari = 100;
+        System.out.println(pi*2*jari);
+    }
+}
