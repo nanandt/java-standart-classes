e1fb5e8aa16c7c26d930e50bfee68ca9f8c18fb7
nanandt <rizkyfatih007@gmail.com>
2021-04-28 21:53:48 +0700

    System dan Runtime class

---
+++ src/rizky/classes/RuntimeApp.java
@@ -0,0 +1,12 @@
+package rizky.classes;
+
+public class RuntimeApp {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println(runtime.availableProcessors());
+        System.out.println(runtime.freeMemory());
+        System.out.println(runtime.maxMemory());
+        System.out.println(runtime.totalMemory());
+    }
+}
