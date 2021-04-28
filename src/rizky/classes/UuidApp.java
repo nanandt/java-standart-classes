a7d1099415e26bdd3d33bb1fa38740fd933c082a
nanandt <rizkyfatih007@gmail.com>
2021-04-28 22:02:24 +0700

    Uuid dan Base64 class

---
+++ src/rizky/classes/UuidApp.java
@@ -0,0 +1,14 @@
+package rizky.classes;
+
+import java.util.UUID;
+
+public class UuidApp {
+    public static void main(String[] args) {
+        for (var i = 0; i < 100; i++) {
+            UUID uuid = UUID.randomUUID();
+            String key = uuid.toString();
+
+            System.out.println(key);
+        }
+    }
+}
